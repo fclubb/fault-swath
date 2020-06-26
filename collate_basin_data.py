27b8fa6550345be3beeecacf8d7f14cf4ee66cc8
fclubb <f.clubb25@gmail.com>
2020-06-26 09:14:33 +0100

    Basin orientation script is working

--- collate_basin_data.py
+++ collate_basin_data.py
@@ -20,10 +20,12 @@ from shapely.geometry import shape, Polygon, mapping, Point, LineString
 import geopandas as gpd
 import math
 
-def azimuth(point1, point2):
+def azimuth(row):
     """
-    azimuth between 2 shapely points.
+    azimuth between 2 shapely points from a geodataframe row
     """
+    point1 = row['centroids']
+    point2 = row['outlet_coords']
     angle = math.atan2(point2.x - point1.x, point2.y - point1.y)
     initial_az = math.degrees(angle)
 
@@ -31,10 +33,10 @@ def azimuth(point1, point2):
     # from -180° to + 180° which is not what we want for a compass bearing
     # The solution is to normalize the initial bearing as shown below
     final_az = (initial_az + 360) % 360
-    print(initial_az, final_az)
+    #print(initial_az, final_az)
     return final_az
 
-def get_basin_orientation(basin_shp):
+def get_basin_orientation(DataDirectory, basin_name):
     """
     This function takes the basin shapefile and gets the orientation of each basin.
     It calculates the minimum bounding rectangle (MBR) of each basin and then finds the
@@ -43,50 +45,32 @@ def get_basin_orientation(basin_shp):
     FJC 24/06/20
     """
 
-    # read in the outlet coords
-    outlets = pd.read_csv('/home/bjdd72/TopographicData/TopographicData/san_andreas/NorthernSAF/tile_3/tile_3_hillslopes_SO3.csv')
-    points = outlets['latitude_outlet']
+    # read in the outlet coords to a geodataframe
+    df = pd.read_csv(DataDirectory+basin_name+'/'+basin_name+'_hillslopes_SO3.csv')
+    geometry = [Point(xy) for xy in zip(df.longitude_outlet, df.latitude_outlet)]
+    crs = 'epsg:4326' #http://www.spatialreference.org/ref/epsg/2263/
+    gdf_outlets = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+    # convert to UTM10
+    gdf_outlets = gdf_outlets.to_crs("EPSG:32610")
 
-    print(outlets)
-
-
-
-    # read in the shapefile to a geopandas dataframe
-    gdf = gpd.read_file(basin_shp)
+    # read in the shapefile to a geodataframe
+    gdf = gpd.read_file(DataDirectory+'/'+'basin_shapefiles/'+basin_name+'_basins.shp')
     # get centroids of each polygon
     gdf['centroids'] = gdf['geometry'].centroid
 
-    # TODO
-    # read outlets csv to a gdf
     # join outlets gdf with basins gdf on basin ID
+    gdf = gdf.merge(gdf_outlets, left_on='DN', right_on='basin_id')
+    # rename the geometry columns
+    gdf = gdf.rename(columns={'geometry_x':'geometry', 'geometry_y': 'outlet_coords'})
+    print(gdf.columns)
+
     # calculate aziumth between centroid and outlet (outlet as 2nd point) to get orientation of the basin.
-    print(gdf)
-    # for rec in c:
-    #     polygon = Polygon(shape(gdf['geometry']))
-    #     # get the minimum bounding rectangle and zip coordinates into a list of point-tuples
-    #     mbr_points = list(zip(*polygon.minimum_rotated_rectangle.exterior.coords.xy))
-    #     print(polygon.minimum_rotated_rectangle.exterior.coords.xy)
-    #
-    #     # calculate the length of each side of the minimum bounding rectangle
-    #     axes = [LineString((mbr_points[i], mbr_points[i+1])) for i in range(len(mbr_points) -1)]
-    #     mbr_lengths = [ax.length for ax in axes]
-    #     #print(mbr_lengths)
-    #
-    #     # get major/minor axis measurements
-    #     minor_axis = min(mbr_lengths)
-    #     major_axis = max(mbr_lengths)
-    #     # find the index of the major axis and get the shapely line of this axis
-    #     major_idx = mbr_lengths.index(max(mbr_lengths))
-    #     major_ax = axes[major_idx]
-    #     #print(major_ax.coords[0])
-    #
-    #     # now calculate the bearing from the first and last point of the line
-    #     az = azimuth(Point(major_ax.coords[0]), Point(major_ax.coords[-1]))
-    #     rec['azimuth'] = az
-        #print(az)
-    #gdf.to_file(DataDirectory+'tile_3_basins_az.shp')
+    gdf['azimuth'] = gdf.apply(azimuth, axis=1)
+    final_gdf = gpd.GeoDataFrame(gdf[['basin_id', 'azimuth', 'latitude_outlet', 'longitude_outlet', 'slope_median', 'slope_16th', 'slope_84th']], geometry=gdf['geometry'], crs='EPSG:32610')
+    # now save the gdf to a shapefile for checking
+    final_gdf.to_file(DataDirectory+'/'+'basin_shapefiles/'+basin_name+'_basins_az.shp')
 
 
-DataDirectory = '/home/bjdd72/TopographicData/TopographicData/san_andreas/NorthernSAF/basin_shapefiles/'
-basin_shp = 'tile_3_basins.shp'
-get_basin_orientation(DataDirectory+basin_shp)
+DataDirectory = '/home/bjdd72/TopographicData/TopographicData/san_andreas/NorthernSAF/'
+basin_name = 'tile_3'
+get_basin_orientation(DataDirectory, basin_name)
